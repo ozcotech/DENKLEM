d43c7324c19fd24600b77eac63d2d7d79014273d
ozcotech <ozkancomert@hotmail.com>
2025-05-14 22:27:38 +0300

    chore(config): Update configuration files for React 19 and RN 0.79.2 compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path'); // Bu satırı ekleyin
 
 /**
  * Metro configuration
@@ -6,6 +7,24 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => {
+          // Bu, projenizin node_modules klasörüne doğru bir şekilde işaret eder
+          return path.join(__dirname, `node_modules/${name}`);
+        },
+      },
+    ),
+  },
+  watchFolders: [
+    path.resolve(__dirname), // Proje kök dizinini izle
+    // Eğer monorepo yapınız varsa veya dışarıdan linklenmiş paketleriniz varsa
+    // buraya ek yollar ekleyebilirsiniz. Örneğin:
+    // path.resolve(__dirname, '../another-package'),
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
